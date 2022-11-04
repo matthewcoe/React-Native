9689a77055cad6796a54039fb47c6cd1968e545a
Matthew Coe <matthewcoe001@gmail.com>
2022-11-04 01:42:48 -0600

    Navigation

--- screens/DirectoryScreen.js
+++ screens/DirectoryScreen.js
@@ -1,10 +1,18 @@
+import { useState } from 'react';
 import { FlatList } from 'react-native';
 import { Avatar, ListItem } from 'react-native-elements';
+import { CAMPSITES } from '../shared/campsites';
 
-const DirectoryScreen = (props) => {
+const DirectoryScreen = ({ navigation }) => {
+    const [campsites, setCampsites] = useState(CAMPSITES);
+
     const renderDirectoryItem = ({ item: campsite }) => {
         return (
-            <ListItem onPress={() => props.onPress(campsite.id)}>
+            <ListItem
+                onPress={() =>
+                    navigation.navigate('CampsiteInfo', { campsite })
+                }
+            >
                 <Avatar source={campsite.image} rounded />
                 <ListItem.Content>
                     <ListItem.Title>{campsite.name}</ListItem.Title>
@@ -14,15 +22,14 @@ const DirectoryScreen = (props) => {
                 </ListItem.Content>
             </ListItem>
         );
-    }
-
+    };
     return (
         <FlatList
-            data={props.campsites}
+            data={campsites}
             renderItem={renderDirectoryItem}
             keyExtractor={(item) => item.id.toString()}
         />
-    )
-}
+    );
+};
 
 export default DirectoryScreen;
