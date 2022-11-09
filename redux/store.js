26b53490e664fd52f4b9df0616f05ffac5debd31
Matthew Coe <matthewcoe001@gmail.com>
2022-11-09 11:35:01 -0700

    Redux Adding Favorites

--- redux/store.js
+++ redux/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { campsitesReducer } from '../features/campsites/campsitesSlice';
 import { commentsReducer } from '../features/comments/commentsSlice';
+import { favoritesReducer } from '../features/favorites/favoritesSlice';
 import { partnersReducer } from '../features/partners/partnersSlice';
 import { promotionsReducer } from '../features/promotions/promotionsSlice';
 
@@ -8,6 +9,7 @@ export const store = configureStore({
     reducer: {
         campsites: campsitesReducer,
         comments: commentsReducer,
+        favorites: favoritesReducer,
         partners: partnersReducer,
         promotions: promotionsReducer
     }
