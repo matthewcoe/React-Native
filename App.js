cc77b3c409128212df793976a054f87402efd5b8
Matthew Coe <matthewcoe001@gmail.com>
2022-11-12 10:58:55 -0700

    Workshop 3

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import Main from './screens/MainComponent';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
+import { LogBox } from 'react-native';
+
+LogBox.ignoreAllLogs(true)
 
 const MyTheme = {
   dark: true,
