3a680dafce32865e7b908961b76bc65fd7fae693
Matthew Coe <matthewcoe001@gmail.com>
2022-11-17 17:34:27 -0700

    Persist Redux Store

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import Main from './screens/MainComponent';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
 import { LogBox } from 'react-native';
+import { Provider } from 'react-redux';
+import { persistor, store } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import Loading from './components/LoadingComponent';
 
 LogBox.ignoreAllLogs(true)
 
@@ -19,9 +21,11 @@ const MyTheme = {
 export default function App() {
   return (
     <Provider store={store}>
-      <NavigationContainer theme={MyTheme}>
-        <Main />
-      </NavigationContainer>
+      <PersistGate loading={<Loading />} persistor={persistor}>
+        <NavigationContainer theme={MyTheme}>
+          <Main />
+        </NavigationContainer>
+      </PersistGate>
     </Provider>
   );
 }
--- redux/store.js
+++ redux/store.js
@@ -4,13 +4,45 @@ import { commentsReducer } from '../features/comments/commentsSlice';
 import { favoritesReducer } from '../features/favorites/favoritesSlice';
 import { partnersReducer } from '../features/partners/partnersSlice';
 import { promotionsReducer } from '../features/promotions/promotionsSlice';
+import {
+    persistStore,
+    PersistCombineReducers,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER
+} from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import persistCombineReducers from 'redux-persist/es/persistCombineReducers';
+
+const config = {
+    key: 'root',
+    storage: AsyncStorage,
+    debug: true
+};
 
 export const store = configureStore({
-    reducer: {
+    reducer: persistCombineReducers(config, {
         campsites: campsitesReducer,
         comments: commentsReducer,
         favorites: favoritesReducer,
         partners: partnersReducer,
         promotions: promotionsReducer
-    }
+    }),
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [
+                    FLUSH,
+                    REHYDRATE,
+                    PAUSE,
+                    PERSIST,
+                    PURGE,
+                    REGISTER
+                ]
+            }
+        })
 });
+export const persistor = persistStore(store);
